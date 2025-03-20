c9f1045bfaf5cd002f1aab8ae1332c60d490ea62
Nigel <nigelbyrnedesign@gmail.com>
2025-03-20 15:27:23 +0000

    user model TDD

---
+++ test/fixtures.js
@@ -0,0 +1,28 @@
+export const michael = {
+    firstName: "Michael",
+    lastName: "Jackson",
+        email: "michael@jackson.ie",
+        password: "password",
+  };
+
+
+export const testUsers = [
+    {
+      firstName: "John",
+      lastName: "Jackson",
+      email: "john@jackson.com",
+      password: "password",
+    },
+    {
+      firstName: "Jack",
+      lastName: "Johnson",
+      email: "jack@johnson.com",
+      password: "password",
+    },
+    {
+      firstName: "Michael",
+      lastName: "Johnson",
+      email: "michael@johnson.com",
+      password: "password",
+    },
+  ];
